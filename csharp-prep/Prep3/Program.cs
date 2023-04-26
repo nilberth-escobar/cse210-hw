3fa59bf638a51454ef447cf90baf3445a95fe6f1
nilberth-escobar <nilberrhescobar@gamil.com>
2023-04-26 12:33:21 -0600

    Asking if the user wants to continue

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Prep3");
+
+        int guessNumber = 18;
+
+        Console.WriteLine("Welcome to Guess the number");
+
+        //asking the user to enter the number
+
+        //int userNumber = 0;
+        string userAnswer = "";
+
+        while (userAnswer.ToLower() != "no")
+        {
+            Console.WriteLine("Enter your guessing number:");
+
+            int Number = int.Parse(Console.ReadLine());
+
+            if (Number > guessNumber)
+            {
+                Console.WriteLine("Guess a lower number...");
+            }
+
+            else if (Number < guessNumber)
+            {
+                Console.WriteLine("Guess a higher number...");
+            }
+
+            else
+            {
+                Console.WriteLine("You got it");
+
+                
+            }
+
+            Console.WriteLine("\nWould you like to keep playing?");
+            userAnswer = Console.ReadLine();
+            
+        }
+
     }
 }
