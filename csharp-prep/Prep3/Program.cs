9e20a2d89699a1137acb353307acb8959fdbab40
nilberth-escobar <nilberrhescobar@gamil.com>
2023-04-27 13:10:18 -0600

    Number generated ramdoml

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,27 +6,33 @@ class Program
     {
         Console.WriteLine("Prep3");
 
-        int guessNumber = 18;
+        //int guessNumber = 18;
 
-        Console.WriteLine("Welcome to Guess the number");
+        Console.WriteLine("\nWelcome to Guess the number");
+        Console.WriteLine("\nThe number you have to guess is generated automatically");
 
-        //asking the user to enter the number
+        Random randomNumber = new Random();
+        int computerNumber = randomNumber.Next(1, 101);
 
+        
         //int userNumber = 0;
+
+        //string variable that saves the user answers if keep playing
         string userAnswer = "";
 
         while (userAnswer.ToLower() != "no")
         {
-            Console.WriteLine("Enter your guessing number:");
+            //asking the user to enter the number
+            Console.WriteLine("\nEnter your guessing number:");
 
             int Number = int.Parse(Console.ReadLine());
 
-            if (Number > guessNumber)
+            if (Number > computerNumber)
             {
                 Console.WriteLine("Guess a lower number...");
             }
 
-            else if (Number < guessNumber)
+            else if (Number < computerNumber)
             {
                 Console.WriteLine("Guess a higher number...");
             }
@@ -34,7 +40,6 @@ class Program
             else
             {
                 Console.WriteLine("You got it");
-
                 
             }
 
