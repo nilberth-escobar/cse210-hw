fa0df1ae6389cec86a61ab7de708bb37ee75897c
nilberth-escobar <nilberrhescobar@gamil.com>
2023-05-31 23:10:14 -0600

    Working fine

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,44 @@
-using System;
+/*using System;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+    }
+}*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Assignment assig1 = new Assignment();
+        assig1.SetStudentName("Jorge Benitez");
+        assig1.SetTopic("Multiplication");
+        
+        Console.WriteLine(assig1.GetSummary());
+
+        MathAssignment math = new MathAssignment();
+        math.SetStudentName("Roberto Rodriquez");
+        math.SetTopic("Fractions");
+        math.SetTextBookSection("Section 7.3");
+        math.SetProblems("Problems 8-19");
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment write = new WritingAssignment();
+        write.SetTitle("The Causes of World War II");
+        write.SetStudentName("Mary Waters");
+        write.SetTopic("European History");
+
+        Console.WriteLine(write.GetSummary());
+        Console.WriteLine(write.GetWritingInformation());
+
+
+
     }
 }
